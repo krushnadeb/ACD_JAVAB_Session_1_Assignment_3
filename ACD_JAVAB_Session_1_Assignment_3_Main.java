6f2fc07c8de7279f11d2e33dedfc6874eab4222e
krushnadeb <krushna_deb@rediffmail.com>
2016-04-16 19:58:38 +0530

    ACD_JAVAB_Session_1_Assignment_3_Main

---
+++ ACD_JAVAB_Session_1_Assignment_3_Main.java
@@ -0,0 +1,11 @@
+//Give two examples of data that cannot be stored in a variable of type int.
+public class ACD_JAVAB_Session_1_Assignment_3_Main {
+	public static void main(String[] args) {	
+		int a;
+		//a=123.12;//float value can't be stored int
+		//a="A string cannot be stored in an int";
+		System.out.println("Give two examples of data that cannot be stored in a variable of type int.");			
+		System.out.println("a=123.12;//float value can't be stored int");			
+		System.out.println("a=\"A string cannot be stored in an int\";");			
+		}
+}
